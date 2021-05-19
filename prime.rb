61d568d412d5e07df4d48bec07139102459edf32
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-19 17:43:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 # Add  code here!
 def prime?(int)
-  if int <= 
-  (2..(int - 1)).each do |n|
-    return false if num % n == 0
-  end
+  if int <= 1 || int == 0
+    return false
+    elsif 
+      (2..(int - 1)).each do |n|
+        return false if num % n == 0
+      end
+    end
   true
 end
   
