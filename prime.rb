9dd8add8d91931c2e7916822c59d52e50938e05b
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-19 17:45:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(int)
+  if int <= 1 
+    return false
+    elsif 
+      (2..(int - 1)).each do |n|
+        return false if int % n == 0
+      end
+    end
+  true
+end
+  
